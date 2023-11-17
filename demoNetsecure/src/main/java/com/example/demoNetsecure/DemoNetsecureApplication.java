9a8bf9517b8f5fd9b54b906461fe72c7cb3be3d0
MeriemBchini <MeriemBchini@Netsecure.com>
2023-11-17 12:13:40 +0100

    initialazing spring boot

---
+++ demoNetsecure/src/main/java/com/example/demoNetsecure/DemoNetsecureApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoNetsecure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNetsecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNetsecureApplication.class, args);
+	}
+
+}
---
+++ demoNetsecure/src/test/java/com/example/demoNetsecure/DemoNetsecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoNetsecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoNetsecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
