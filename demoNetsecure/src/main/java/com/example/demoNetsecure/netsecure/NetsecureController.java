0a987cda4641afdd0ff6d7f361ba717c402e5adb
MeriemBchini <MeriemBchini@Netsecure.com>
2023-11-19 15:12:08 +0100

    adding sample things

---
+++ demoNetsecure/src/main/java/com/example/demoNetsecure/netsecure/NetsecureController.java
@@ -0,0 +1,8 @@
+package com.example.demoNetsecure.netsecure;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RequestMapping("/api/v1/netsecures")
+@RestController
+public class NetsecureController {
+}
